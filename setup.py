4971b8281df0dcf9a6c3f6993bc4b194f37c2e04
Paul Barton <pablo.barton@gmail.com>
2013-12-14 20:15:23 -0500

    Minor touchups in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
-import sys
 import os
+
 
 def long_description():
     rfname = os.path.join(os.path.dirname(__file__), 'README.md')
@@ -10,13 +10,13 @@ def long_description():
     return(readme_text)
 
 setup(name='FileTranscriber',
-      version='0.2.0',
+      version='0.2.1',
       description='''A small utility that simulates user typing to aid \
 file transcription in limited environments''',
       long_description=long_description(),
       author='Paul Barton',
       author_email='pablo.barton@gmail.com',
       url='https://github.com/SavinaRoja/FileTranscriber',
-      install_requires = ['docopt', 'PyUserInput'],
+      install_requires=['docopt', 'PyUserInput'],
       license='http://opensource.org/licenses/MIT',
       scripts=['transcribe'])
